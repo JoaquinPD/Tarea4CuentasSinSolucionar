e00572d200918b380936fc462edea92de0d214e6
Joaquin <Joaquin@DESKTOP-8B64I53>
2023-02-03 21:30:08 +0100

    prueba3

--- src/cuentas/cliente/Main.java
+++ src/cuentas/cliente/Main.java
@@ -10,19 +10,19 @@ public class Main {
         Cuenta cuenta1;
         double saldoActual;
 
-        cuenta1 = new Cuenta("Rafael Nadal Parera","1000-2365-85-1230456789",2500,0);
+        cuenta1 = new Cuenta("Rafael Nadal Parera","1000-2365-85-1230456789",-200,0);
         
         System.out.println("El saldo actual es:"+ cuenta1.estado() );
 
         try {
-            cuenta1.retirar(500);
+            cuenta1.retirar(100);
             System.out.println("El saldo actual es:"+ cuenta1.estado() );
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            cuenta1.ingresar(-1000);
+            cuenta1.ingresar(600);
             System.out.println("El saldo actual es:"+ cuenta1.estado() );
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
